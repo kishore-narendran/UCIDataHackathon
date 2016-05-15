b4a3753b347719e852c9120d9fa5c3b1231fa3ee
shiladitya <shiladis@uci.edu>
2016-05-15 16:53:49 -0700

    Adding files for getting training data and learning models

--- DataModel/blackBoxModel.py
+++ DataModel/blackBoxModel.py
@@ -1,5 +1,39 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error as mse
+import msgpack
+from datetime import datetime
 
-blackBox = LinearRegression(normalize=True)
-blackBox.fit(X, y)
+dataFile = open('../Data/device_app_mappings', 'rb')
+device_app_mapping = msgpack.unpackb(dataFile.read())
+dataFile.close()
+
+models = {}
+
+for deviceType, appIDs in device_app_mapping.items():
+	assert (deviceType not in models), 'Repeated device type'
+	models[deviceType] = {}
+	print deviceType
+
+	for appID in appIDs:
+		try:
+			X, y, ids = log_dataBase[deviceType][appID]
+			blackBox = LinearRegression(normalize=True)
+			blackBox.fit(X, y)
+
+			p = blackBox.predict(X)
+			model = (blackBox.coef_, blackBox.intercept_, blackBox.mse(y,p)/len(y))
+			models[deviceType][appID] = model
+		except:
+			pass
+
+file_name = 'blackBoxes'
+print '========================================================================'
+print 'Started packing into Msgpack ', file_name
+print '========================================================================'
+handle = open(file_name.split('.')[0] + '_complete', 'wb')
+handle.write(msgpack.packb(models, default=m.encode))
+handle.close()
+print '========================================================================'
+print 'Finished packing into Msgpack ', file_name
+print '========================================================================'
--- DataModel/createDataSet.py
+++ DataModel/createDataSet.py
@@ -1,13 +1,9 @@
 import numpy as np
 import msgpack
 from datetime import datetime
+import msgpack_numpy as m
 
-deviceType = 26126
-appID = 28893190
-
-fileName = '../Data/log_data_september_first_half_complete'
-
-schemaIX =	{
+log_schemaIX =	{
 				'battery' : 0,
 				'back_battery' : 1,
 				'cpu' : 2,
@@ -22,46 +18,71 @@ schemaIX =	{
 				'data_size' : 11,
 				'cache_size' : 12,
 				'other_size' : 13
-		 	}
+		 		}
 
-# open database
-dataFile = open(fileName, 'rb')
-database = msgpack.unpackb(dataFile.read())
+log_fileNames = ['../Data/log_data_september_first_half_complete', '../Data/log_data_september_second_half_complete']
+databases = []
+
+for log_fileName in log_fileNames:
+	dataFile = open(log_fileName, 'rb')
+	half_database = msgpack.unpackb(dataFile.read())
+	dataFile.close()
+	print 'Finished loading', log_fileName
+
+	databases += [half_database]
+
+print 'Finished loading all log databases into memory'
+
+dataFile = open('../Data/device_app_mappings', 'rb')
+device_app_mapping = msgpack.unpackb(dataFile.read())
 dataFile.close()
 
-try:
-	data = (database[deviceType])[appID]
-	data_schema = 	[
-						'cpu',
-						'back_cpu',
-						'run_time',
-						'front_run_time',
-						'back_data',
-						'data_wifi',
-						'data_mobile',
-						'memory',
-						'data_size',
-						'cache_size',
-						'other_size'
-					]
+log_dataBase = {}
 
-	#create datasets
-	timestamps = []
-	X = []
-	y = []
+for deviceType, appIDs in device_app_mapping.items():
+	assert (deviceType not in log_dataBase), 'Repeated device type'
+	log_dataBase[deviceType] = {}
+	print deviceType
 
-	for timestamp, entry in data.items():
-		timestamps += [datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S')]
-		y += [ entry[schemaIX['battery']] * entry[schemaIX['run_time']] ]
+	for appID in appIDs:
+		dataPt_keys = []
+		X = []
+		y = []
 
-		dataPt = []
-		for field in data_schema:
-			dataPt += [entry[schemaIX[field]]]
+		for database in databases:
+			try:
+				data = (half_database[deviceType])[appID]
+				log_data_schema = 	[
+									'cpu',
+									'back_cpu',
+									'run_time',
+									'front_run_time',
+									'back_data',
+									'data_wifi',
+									'data_mobile',
+									'memory',
+									'data_size',
+									'cache_size',
+									'other_size'
+									]
 
-		X += [np.asarray(dataPt)]
+				for device_id, log in data.items():
+					for timestamp, entry in log.items():
+						dataPt_keys += [ (device_id, datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S')) ]
+						y += [ entry[log_schemaIX['battery']] + entry[log_schemaIX['back_battery']] ]
 
-	X = np.asarray(X)
-	y = np.asarray(y)
+						dataPt = []
+						for field in log_data_schema:
+							dataPt += [entry[log_schemaIX[field]]]
 
-except:
-	print 'App not found on any such device'
+						X += [np.asarray(dataPt)]
+			except:
+				#print 'App', appID, 'not found on device type', deviceType
+				pass
+
+		X = np.asarray(X)
+		y = np.asarray(y)
+
+		assert (appID not in log_dataBase[deviceType].keys()), 'Repeated app ID'
+		log_dataBase[deviceType][appID] = (X, y, dataPt_keys)
+		#print '\t',appID
